3ec29be88f25e84337cc5a573f6ece8a9d1464b3
ANASTASIA COTOROBAI p1713175 <cotorobai.anastasia@etu.univ-lyon1.fr>
2018-05-11 15:04:29 +0200

    commit after reset all hard

--- LIFAP5-Projet.js
+++ LIFAP5-Projet.js
@@ -10,58 +10,136 @@ const api_key_value = 'ad1939d50a6138a47ba4';
 let headers = new Headers();
 headers.set(api_header, api_key_value);
 
-const base_url = "localhost:8888";
-const local_todos = "./Projet-2018-todos.json";
-const local_users = "./Projet-2018-users.json";
-
+const user_name = 'p1508726';
+const url_user = 'http://localhost:8888/index.php/users/';
 const url = 'http://localhost:8888/index.php/todos/';
 
-const mon_todo = {
-    'title': 'un nouveau todo',
-    'desc': "Description du nouveau todo",
-    'people': []
-};
-
-const data = JSON.stringify(mon_todo);
-
-fetch(url, { method: 'GET', headers: headers})
-    .then(function(response) {
-        console.log("hey");
-        if (response.ok) {
-
-            return response.json();
-        } else {
-            console.log(`Erreur dans la requête ${url}: ${response.code}`);
-            throw("Erreur lors de la requête sur le serveur");
-        }
+function filtreTexte(requete,table) {
+  return table.filter(function (el) {
+    if (el['createdBy'].toLowerCase().indexOf(requete.toLowerCase()) > -1)
+    {
+      return 1;
+    }
+    let test = el['people'].filter(function (el2){
+      return el2.toLowerCase().indexOf(requete.toLowerCase()) > -1;
     })
-    .then(function(todoEnJson) {
-        // faireQuelqueChoseAvecLesDonnees(todoEnJson);
-        console.log(todoEnJson);
-        todoEnJson.map(showContent);
+    if(test.length != 0)
+    {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  })
+}
+
+function fetch_info()
+{
+    fetch(url, { method: 'GET', headers: headers})
+        .then(function(response) {
+            console.log("hey");
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log(`Erreur dans la requête ${url}: ${response.code}`);
+                throw("Erreur lors de la requête sur le serveur");
+            }
+        })
+        .then(function(todoEnJson) {
+            // faireQuelqueChoseAvecLesDonnees(todoEnJson);
+
+            let my= filtreTexte('p1713175',todoEnJson);
+
+            let new_array = my.map(obj => obj);
+          //  console.log(new_array);
+
+            $(document).ready(function(){
+                $(".deadline").click(function(){
+                    console.log("salut");
+                    new_array.sort(function (o1,o2){
+                        return new Date(o1["deadline"])-new Date(o2["deadline"]);
+                    })
+                    $(".list-group-item").remove();
+                    new_array.map(import_json)
+
+                });
+            });
+            $(document).ready(function(){
+                $(".title").click(function(){
+                    console.log("salut");
+                    new_array.sort(function(a, b){
+                        if(a.title.toUpperCase() < b.title.toUpperCase()) return -1;
+                        if(a.title.toUpperCase() > b.title.toUpperCase()) return 1;
+                        return 0;
+                    })
+                    $(".list-group-item").remove();
+                    new_array.map(import_json)
+
+                });
+            });
+            new_array.map(import_json)
 
 
-    });
+        });
 
+}
+function fetch_users(users)
+{
+    fetch(url_user + users.id, { method: 'GET', headers: headers })
+        .then(function(response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.log(`Erreur dans la requête ${url}: ${response.code}`);
+                throw("Erreur lors de la requête sur le serveur");
+            }
+        })
+        .then(function(user_info) {
+            document.getElementById("popup_id").style.display="block";
+            document.getElementsByClassName("container-fluid")[0].style.WebkitFilter = 'blur(4px)';
+            document.getElementById("image_user").src="img/img_avatar.png";
+            document.getElementById("nom_user").innerHTML=user_info["_id"];
+            document.getElementById("email_user").innerHTML=user_info["email"];
+            document.getElementById("date_user").innerHTML=user_info["joinedOn"];
+        });
 
-function showContent(todoEnJson) {
-    let temp = document.getElementsByTagName("template")[0];
-    console.log(temp.content.querySelector('li'));
-    temp.content.querySelector('li .content').innerHTML = todoEnJson['title'];
-    temp.content.querySelector('li div div').id = todoEnJson['_id'];
-    temp.content.querySelector('.expend').href = "#"+todoEnJson['_id'];
-    temp.content.querySelector('#desc').innerHTML = todoEnJson['desc'];
-    temp.content.querySelector('#deadline').innerHTML = todoEnJson['deadline'];
-    temp.content.querySelector('#people').innerHTML = todoEnJson['people'];
+}
 
-    let clon = temp.content.cloneNode(true);
-    document.getElementById(todoEnJson['status']).appendChild(clon);
+function usersmask()
+{
+    document.getElementById("popup_id").style.display="none";
+    document.getElementsByClassName("container-fluid")[0].style.WebkitFilter = 'blur(0px)';
 
 }
 
 
+
+function people_div(element)
+{
+   let new_div =  "<a href=\"#\" onclick=\"fetch_users(this)\"  id = " +element+">";
+  // new_div.onmouseout = function(){document.getElementById("popup_id").style.display="none";};
+   new_div+=element;
+   new_div+="</a><br>";
+   document.getElementsByTagName("template")[0].content.querySelector('#people').innerHTML += new_div;
+}
+fetch_info();
+
+    function import_json(todoEnJson) {
+        let temp = document.getElementsByTagName("template")[0];
+        temp.content.querySelector('li .content').innerHTML = todoEnJson['title'];
+        temp.content.querySelector('li div div').id = todoEnJson['_id'];
+        temp.content.querySelector('.expend').href = "#"+todoEnJson['_id'];
+        temp.content.querySelector('#desc').innerHTML = todoEnJson['desc'];
+        temp.content.querySelector('#deadline').innerHTML = todoEnJson['deadline'];
+        temp.content.querySelector('#people').innerHTML="";
+        todoEnJson['people'].map(people_div);
+        let clon = temp.content.cloneNode(true);
+        document.getElementById(todoEnJson['status']).appendChild(clon);
+
+    }
+
 ////////////////////////////////////////////////////////////////////////////////
-// ETAT : classe d'objet pour gérer l'état courant de l'application
+// ETAT : classe d'objet pour gérer l'état courant de l'application (inutilisee)
 ////////////////////////////////////////////////////////////////////////////////
 
 function State(users = [], todos = [], filters = [], sort = "NONE"){
@@ -90,21 +168,17 @@ function State(users = [], todos = [], filters = [], sort = "NONE"){
 }//end State
 
 
-////////////////////////////////////////////////////////////////////////////////
-// OUTILS : fonctions outils, manipulation et filtrage de TODOs
-////////////////////////////////////////////////////////////////////////////////
-
 
 function showTodo()
 {
-        if(document.getElementById("TodoId").style.visibility=="visible") {
-            document.getElementById("TodoId").style.visibility = "collapse";
-            document.getElementById("butontodo").style.color = "grey";
-        }
-        else {
-            document.getElementById("TodoId").style.visibility = "visible";
-            document.getElementById("butontodo").style.color = "red";
-        }
+    if(document.getElementById("TodoId").style.visibility=="visible") {
+        document.getElementById("TodoId").style.visibility = "collapse";
+        document.getElementById("butontodo").style.color = "grey";
+    }
+    else {
+        document.getElementById("TodoId").style.visibility = "visible";
+        document.getElementById("butontodo").style.color = "red";
+    }
 }
 function showDoing()
 {
@@ -131,94 +205,115 @@ function showDone()
 }
 
 function addFunction() {
-        let liste= document.createElement("li");
-        let div = document.createElement("div");
-        let div2 = document.createElement("div");
-        let p1 =document.createElement("p");
-        let p2 =document.createElement("p");
-        let p3 =document.createElement("p");
-        let test="list";
-    		test +=document.getElementById("upload-state").value;
-        let id=document.getElementById(test).getElementsByTagName("li").length+1;
-        liste.className="list-group-item";
-		    liste.innerHTML=document.getElementById("upload-desc").value;
-		    div.className="item-option";
-        let divid="collapse"
-        divid+=id;
-        divid+=document.getElementById("upload-state").value;
-        div2.id=divid;
-        div2.className="panel-collapse collapse";
-        div2.setAttribute("role","tabpanel");
-        div2.setAttribute("aria-labelledby","headingTwo");
-        p1.innerHTML=document.getElementById("upload-mentioned").value;
-        p2.innerHTML=document.getElementById("upload-deadline").value;
-        p3.innerHTML=document.getElementById("upload-state").value;
-        div2.appendChild(p1);
-        div2.appendChild(p2);
-        div2.appendChild(p3);
-        div.innerHTML = "<a role=\"button\" class=\"nav-link\" data-toggle=\"collapse\" href=\"#"+divid+"\" aria-expanded=\"true\" aria-controls="+divid+" class=\"trigger collapsed\"><i class=\"fal fa-arrows-alt\"></i></a><a role=\"button\" onclick=\"editTodo(this)\" data-toggle=\"modal\" data-target=\"#modifieModal\"><i class=\"fas fa-edit\"></i></a><a role=\"button\" class=\"nav-link\" onclick='DeleteTodo(this,this.parentNode.parentNode.parentNode.id)'><i class=\"fas fa-trash-alt\"></i></a>"
-        div.appendChild(div2);
-        liste.appendChild(div);
-        document.getElementById(test).appendChild(liste);
-}
 
+  const mon_todo ={
+    "_id":"",
+    "deadline" : "",
+    "creation" : "",
+    "title" : "",
+    "desc" : "" ,
+    "status" : "",
+    "createdBy" : "",
+    "people" : []
+  }
+  const array = document.getElementById("upload-mentioned").value.split(",")
+  mon_todo["people"] = array;
+  mon_todo["createdBy"]='P1713175';
+  mon_todo["deadline"]=document.getElementById("upload-deadline").value;
+  mon_todo["title"]=document.getElementById("upload-title").value;
+  mon_todo["desc"]=document.getElementById("upload-desc").value;
+  mon_todo["status"]=document.getElementById("upload-state").value
+
+  const data = JSON.stringify(mon_todo);
+
+  fetch(url, { method: 'POST', headers: headers,body: data})
+      .then(function(response) {
+          if (response.ok) {
+
+              return response.json();
+          } else {
+              console.log(`Erreur dans la requête ${url}: ${response.code}`);
+              throw("Erreur lors de la requête sur le serveur");
+          }
+      })
+        .then(function() {
+            $(".list-group-item").remove();
+            fetch_info();
+
+        });
+
+}
 function DeleteTodo(test,frome){
+  fetch(url + test.parentNode.childNodes[7].id, { method: 'DELETE', headers: headers})
+      .then(function(response) {
+          if (response.ok) {
+
+              return response.json();
+          } else {
+              console.log(`Erreur dans la requête ${url2}: ${response.code}`);
+              throw("Erreur lors de la requête sur le serveur");
+          }
+      })
 	document.getElementById(frome).removeChild(test.parentNode.parentNode);
 }
 
+
 let donnee;
 function editTodo(test){
-		    document.getElementById("upload-desc2").value = test.parentNode.parentNode.innerHTML.split('<',1)[0];
-        document.getElementById("upload-mentioned2").value = test.parentNode.childNodes[3].childNodes[0].innerHTML;
-        document.getElementById("upload-deadline2").value = test.parentNode.childNodes[3].childNodes[1].innerHTML;
-        document.getElementById("upload-state2").value = test.parentNode.childNodes[3].childNodes[2].innerHTML;
+        document.getElementById("upload-title2").value = test.parentNode.parentNode.childNodes[1].innerHTML;
+        document.getElementById("upload-desc2").value = test.parentNode.childNodes[7].childNodes[1].innerHTML;
+        document.getElementById("upload-mentioned2").value ="";
+        console.log(test.parentNode.childNodes[7].childNodes[5].childNodes);
+        Array.prototype.map.call(test.parentNode.childNodes[7].childNodes[5].childNodes, function(trie){
+            if (trie.innerHTML != "") {
+                document.getElementById("upload-mentioned2").value += trie.innerHTML;
+                document.getElementById("upload-mentioned2").value += ",";
+            }
+        });
+        document.getElementById("upload-deadline2").value = test.parentNode.childNodes[7].childNodes[3].innerHTML;
         donnee = test;
+
 }
 
 
 function valide()
 {
-  if(donnee.parentNode.childNodes[3].childNodes[2].innerHTML==document.getElementById("upload-state2").value)
-  {
-    donnee.parentNode.childNodes[3].childNodes[0].innerHTML=document.getElementById("upload-mentioned2").value;
-    donnee.parentNode.childNodes[3].childNodes[1].innerHTML=document.getElementById("upload-deadline2").value;
-    donnee.parentNode.childNodes[3].childNodes[2].innerHTML=document.getElementById("upload-state2").value ;
-    donnee.parentNode.parentNode.innerHTML=document.getElementById("upload-desc2").value+'<div class="item-option"><a role="button"'+donnee.parentNode.parentNode.innerHTML.split('<div class="item-option"><a role="button"',2)[1];
-  }
-  else{
-    let liste= document.createElement("li");
-    let div = document.createElement("div");
-    let div2 = document.createElement("div");
-    let p1 =document.createElement("p");
-    let p2 =document.createElement("p");
-    let p3 =document.createElement("p");
-    let test="list";
-    test +=document.getElementById("upload-state2").value;
-    let id=document.getElementById(test).getElementsByTagName("li").length+1;
-    liste.className="list-group-item";
-    liste.innerHTML=document.getElementById("upload-desc2").value;
-    div.className="item-option";
-    let divid="collapse"
-    divid+=id;
-    divid+=document.getElementById("upload-state2").value;
-    div2.id=divid;
-    div2.className="panel-collapse collapse";
-    div2.setAttribute("role","tabpanel");
-    div2.setAttribute("aria-labelledby","headingTwo");
-    p1.innerHTML=document.getElementById("upload-mentioned2").value;
-    p2.innerHTML=document.getElementById("upload-deadline2").value;
-    p3.innerHTML=document.getElementById("upload-state2").value;
-    div2.appendChild(p1);
-    div2.appendChild(p2);
-    div2.appendChild(p3);
-    div.innerHTML = "<a role=\"button\" class=\"nav-link\" data-toggle=\"collapse\" href=\"#"+divid+"\" aria-expanded=\"true\" aria-controls="+divid+" class=\"trigger collapsed\"><i class=\"fal fa-arrows-alt\"></i></a><a role=\"button\" onclick=\"editTodo(this)\" data-toggle=\"modal\" data-target=\"#modifieModal\"><i class=\"fas fa-edit\"></i></a><a role=\"button\" class=\"nav-link\" onclick='DeleteTodo(this,this.parentNode.parentNode.parentNode.id)'><i class=\"fas fa-trash-alt\"></i></a>"
-    div.appendChild(div2);
-    liste.appendChild(div);
-    document.getElementById(test).appendChild(liste);
-    DeleteTodo(donnee,donnee.parentNode.parentNode.parentNode.id);
-  }
-}
 
+  const mon_todo ={
+    "_id":"",
+    "deadline" : "",
+    "creation" : "",
+    "title" : "",
+    "desc" : "" ,
+    "status" : "",
+    "createdBy" : "",
+    "people" : []
+  }
+  const array = document.getElementById("upload-mentioned2").value.split(",")
+  mon_todo["people"] = array;
+  mon_todo["deadline"]=document.getElementById("upload-deadline2").value;
+  mon_todo["title"]=document.getElementById("upload-title2").value;
+  mon_todo["desc"]=document.getElementById("upload-desc2").value;
+  mon_todo["createdBy"]='Romuald';
+  mon_todo["status"]=document.getElementById("upload-state2").value;
+  const data = JSON.stringify(mon_todo);
+  fetch(url, { method: 'POST', headers: headers,body: data})
+      .then(function(response) {
+          console.log("hey");
+          if (response.ok) {
+
+              return response.json();
+          } else {
+              console.log(`Erreur dans la requête ${url}: ${response.code}`);
+              throw("Erreur lors de la requête sur le serveur");
+          }
+      })
+        .then(function(todoEnJson) {
+            DeleteTodo(donnee,donnee.parentNode.parentNode.parentNode.id)
+            $(".list-group-item").remove();
+            fetch_info();
+  })
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // RENDU : fonctions génération de HTML à partir des données JSON
@@ -234,42 +329,8 @@ function valide()
 ////////////////////////////////////////////////////////////////////////////////
 // FETCH Fonction permettant de charger des données asynchrones
 ////////////////////////////////////////////////////////////////////////////////
-/*function get_local_todos() {
-  return fetch(local_todos)
-    .then(response => response.text())
-}
 
-function get_local_users() {
-  return fetch(local_users)
-    .then(response => response.text())
-}
-
-function import_json()
-{
-  get_local_todos()
-  .then((result) => {
-    result = JSON.parse(result);
-    console.log(result);
-    $("#todo1").text(result[0]["title"]);
-  });
-}
-import_json();*/
 
 /************************************************************** */
 /** MAIN PROGRAM */
 /************************************************************** */
-/*document.addEventListener('DOMContentLoaded', function(){
-  let state = {};
-
-  // garde pour ne pas exécuter dans la page des tests unitaires.
-  if (document.getElementById("title-test-projet") == null) {
-
-    Promise.all([get_local_users(),get_local_todos()])
-    .then(values => values.map(JSON.parse))
-    .then(values => new State(values[0], values[1]))
-    .then(state => state.get_user_todos('romuald'))
-    .then(todos => todos.map(x => x['title']))
-    .then(todos => console.log(todos))
-    .catch(reason => console.error(reason));
-  }
-// }, false);*/
